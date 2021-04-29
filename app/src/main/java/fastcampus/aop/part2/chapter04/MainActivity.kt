ef90a4992645d1eb901ecfe5efc1e4526014b67d
kimminkyu <kmk50009@naver.com>
2021-04-29 21:12:55 +0900

    Calculator 화면 구현

--- app/src/main/java/fastcampus/aop/part2/chapter04/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/chapter04/MainActivity.kt
@@ -2,10 +2,27 @@ package fastcampus.aop.part2.chapter04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClicked(v: View){
+
+    }
+
+    fun resultButtonClicked(v: View){
+
+    }
+
+    fun historyButtonClicked(v: View){
+
+    }
+
+    fun clearButtonClicked(v: View){
+
     }
 }
